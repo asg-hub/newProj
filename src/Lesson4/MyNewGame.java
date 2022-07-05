9b56966af87e115da49e43dda60ec8a12188944a
Ahapov Stanislav <agapovstas85@gmail.com>
2022-07-06 01:46:13 +0300

    time to play

---
+++ src/Lesson4/MyNewGame.java
@@ -0,0 +1,29 @@
+package Lesson4;
+
+import java.lang.reflect.Array;
+
+public class MyNewGame {
+    public static void main(String[] args) {
+
+        String[] gameArray = new String[9];
+
+        //инициализация "поля"
+
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = " ";
+        }
+
+        String players1;
+        String players2;
+        String currentPlayers;
+
+        String stringOut = "";
+        for (int i = 0; i < gameArray.length; i++) {
+
+//            stringOut = stringOut + ?(stringOut.length() > 0 , " ", "") + gameArray[i];
+//            System.out.println(gameArray[stringOut]);
+        }
+//        System.out.println(gameArray.);
+
+    }
+}
