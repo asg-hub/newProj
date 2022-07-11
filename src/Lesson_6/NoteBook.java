68663eabcf20f9bc024033873e50e3b4ca0688de
Ahapov Stanislav <agapovstas85@gmail.com>
2022-07-12 02:11:55 +0300

    Game char!

--- src/Lesson_6/NoteBook.java
+++ src/Lesson_6/NoteBook.java
@@ -1,15 +1,43 @@
 package Lesson_6;
 
+import java.util.Scanner;
+
 public class NoteBook {
 
     public static void main(String[] args) {
-        NoteMas noteMas = new NoteMas(10);
+        NoteMas noteMas = new NoteMas();
 
-        noteMas.add("test", 0);
-        noteMas.add("test", 1);
-        noteMas.add("test", 2);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите действие, которое хотите сделать:");
+            System.out.println("1 - если хотите добавить новую запись в записную книжку");
+            System.out.println("2 - если хотите вывести все записи на экран");
+            System.out.println("3 - отсортировать по убыванию даты");
+            System.out.println("4 - отсортировать по возрастанию даты");
+            System.out.println("5 - если хотите завершить работу программы");
+            int res = scanner.nextInt();
+            if (res == 1) {
+                System.out.println("Ок. Введите запись, которую выхотите добавить в книгу:");
+                Scanner scanner2 = new Scanner(System.in);
+                String newNote = scanner2.nextLine();
+                noteMas.add(newNote);
+            } else if (res == 2) {
+                System.out.println("Ок. Смотри что у нас есть:");
+                noteMas.print();
+            } else if (res == 3) {
+                System.out.println("Ок. Сортиркуем по убыванию даты");
+                noteMas.sortDesc();
+            } else if (res == 4) {
+                System.out.println("Ок. Сортиркуем по возрастанию даты");
+                noteMas.sortInc();
+            } else if (res == 5) {
+                System.out.println("Хорошо - прощай!");
+                break;
+            } else {
+                System.out.println("Вы ошиблись - попробуйте снова");
+            }
 
-        noteMas.sort();
-        noteMas.print();
+        }
+
     }
 }
--- src/Lesson_6/NoteMas.java
+++ src/Lesson_6/NoteMas.java
@@ -1,30 +1,100 @@
 package Lesson_6;
 
+import java.util.Date;
+
 public class NoteMas {
     Note[] notes;
-    NoteMas(int col) {
-        this.notes = new Note[col];
-    }
-    void add(String plan, int index) {
+
+    //NoteMas(int col) {
+    //this.notes = new Note[col];
+    //}
+
+    void add(String plan) {
+        rebuildArray();
         Note note = new Note();
-        note.data = "test";
-        note.plan = "test";
-        this.notes[index] = note;
+        note.addCurrentNote(plan);
+        this.notes[this.notes.length - 1] = note;
     }
-    void print(){
+
+    void sortDesc() {
+
+        boolean itSorted = false;
+        Note buf;
+
+        while (!itSorted) {
+            itSorted = true;
+            for (int i = 0; i < this.notes.length - 1; i++) {
+
+                Date date1 = this.notes[i].getDate();
+                Date date2 = this.notes[i + 1].getDate();
+
+                if (date1.before(date2)) {
+                    itSorted = false;
+                    buf = this.notes[i];
+                    this.notes[i] = this.notes[i + 1];
+                    this.notes[i + 1] = buf;
+                }
+
+
+            }
+        }
+    }
+
+    void sortInc() {
+        boolean itSorted = false;
+        Note buf;
+        while (!itSorted) {
+            itSorted = true;
+            for (int i = 0; i < this.notes.length - 1; i++) {
+
+                Date date1 = this.notes[i].getDate();
+                Date date2 = this.notes[i + 1].getDate();
+
+                if (date1.after(date2)) {
+                    itSorted = false;
+                    buf = this.notes[i];
+                    this.notes[i] = this.notes[i + 1];
+                    this.notes[i + 1] = buf;
+                }
+
+
+            }
+        }
+    }
+
+    void rebuildArray() {
+        if (this.notes == null) {
+            this.notes = new Note[1];
+        } else {
+            Note[] tempNotes = this.notes;
+            Note[] newNotes = new Note[tempNotes.length + 1];
+
+            for (int i = 0; i < tempNotes.length; i++) {
+                newNotes[i] = tempNotes[i];
+            }
+            this.notes = newNotes;
+        }
+
+    }
+
+    void print() {
         for (int i = 0; i < this.notes.length; i++) {
             if (this.notes[i] == null) {
                 continue;
             }
-            //System.out.println(this.notes.length);
             System.out.print(i + ": ");
-            System.out.print(this.notes[i].data);
-            System.out.print(" - ");
-            System.out.print(this.notes[i].plan);
+            this.notes[i].print();
             System.out.println();
+            //System.out.println(this.notes.length);
+//            System.out.print(i + ": ");
+//            System.out.print(this.notes[i].data);
+//            System.out.print(" - ");
+//            System.out.print(this.notes[i].plan);
+//            System.out.println();
         }
     }
-    void sort(){
+
+    void sort() {
         System.out.println("sorted");
     }
 }
